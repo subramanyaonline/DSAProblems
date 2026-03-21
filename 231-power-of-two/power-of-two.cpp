4d209a051a3fd2f28dab2e6cf72124d292bea1c1
subramanyaonline <subramanyanayak2003@gmail.com>
2026-03-21 23:02:51 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (49.51%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n<1) return false ;
+        if(n<1) return false ; //if this is not given, this will lead to run time error as, overflow of int , lower bound 2p31-1, overflow. 
         return ((n & (n-1)) == 0) ; 
     }
 };
