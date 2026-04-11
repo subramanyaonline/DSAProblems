6d85da03af10b7f69220fc0debb5d0971c7baca2
subramanyaonline <subramanyanayak2003@gmail.com>
2026-04-11 12:08:15 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (83.78%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        if(p.size()>s.size()) return {} ;
+
+        vector<int> pfreq(26,0) ;
+        for(char c : p) pfreq[c-'a']++ ;
+
+        //creating a base window in s of size k(p.size())
+        vector<int> sfreq(26,0) ;
+        for(int i=0;i<p.size();i++)     sfreq[s[i]-'a']++ ;
+
+        vector<int> ans ;
+        if(isequal(pfreq,sfreq)) ans.push_back(0) ;
+
+        for(int i=p.size();i<s.size();i++){
+            sfreq[s[i] - 'a']++;
+            sfreq[ s[i-p.size()] - 'a']-- ;
+            if(isequal(pfreq,sfreq)) ans.push_back(i-p.size()+1) ;
+        }
+
+        return ans ;
+    }
+
+    bool isequal(vector<int> &pfreq, vector<int> &sfreq){
+        for(int i=0;i<26;i++) if(pfreq[i]!=sfreq[i]) return false ;
+        return true ;
+    }
+};
