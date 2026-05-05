faade60677c28749bc964be4dcb750573258a6cb
subramanyaonline <subramanyanayak2003@gmail.com>
2026-05-05 09:51:38 +0530

    Time: 3 ms (26.26%) | Memory: 9 MB (21.80%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -1,28 +1,51 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        vector<int> freq(26) ;
-        for(int i=0;i<s.size();i++){
-            freq[s[i]-'a']++;
-        }
-        
         vector<int> ans ;
-        unordered_set<char> set ; 
-        vector<int> curfreq(26) ;
-        int count = 0 ;
+        unordered_map<char,int> um ; //int here keeps the last occurancee index
+
         for(int i=0;i<s.size();i++){
-            count++;
-            set.insert(s[i]) ; 
-            int curascii = s[i]-'a' ;
-            curfreq[curascii]++;
-            if(freq[curascii]==curfreq[curascii]){
-                set.erase(s[i]) ;
-                if(set.empty()){
-                    ans.push_back(count) ;
-                    count = 0 ;
-                }
-            }   
+            um[s[i]] = i ;
+        }
+
+        int start = 0 ;
+        int maxx = 0 ; 
+        for(int i=0;i<s.size();i++){
+            maxx = max(um[s[i]],maxx) ;
+            if(maxx<=i){
+                ans.push_back(maxx-start+1) ;
+                start = maxx+1; 
+                maxx = maxx + 1; 
+            }
         }
         return ans ;
+
+
+
+
+
+        // vector<int> freq(26) ;
+        // for(int i=0;i<s.size();i++){
+        //     freq[s[i]-'a']++;
+        // }
+        
+        // vector<int> ans ;
+        // unordered_set<char> set ; 
+        // vector<int> curfreq(26) ;
+        // int count = 0 ;
+        // for(int i=0;i<s.size();i++){
+        //     count++;
+        //     set.insert(s[i]) ; 
+        //     int curascii = s[i]-'a' ;
+        //     curfreq[curascii]++;
+        //     if(freq[curascii]==curfreq[curascii]){
+        //         set.erase(s[i]) ;
+        //         if(set.empty()){
+        //             ans.push_back(count) ;
+        //             count = 0 ;
+        //         }
+        //     }   
+        // }
+        // return ans ;
     }
 };
