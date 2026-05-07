3f677a63ae34e620bbf6f0072f8dd1c688ebf3da
subramanyaonline <subramanyanayak2003@gmail.com>
2026-05-07 14:10:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (90.81%) - LeetSync

---
+++ 778-reorganize-string/reorganize-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        int freq[26] ;  for(int i=0;i<26;i++) freq[i]=0 ;
+        for(int ele : s) freq[ele-'a']++;
+
+        string ans ; 
+        char prevchar = ' ';     //previously pushed/used char to the string 
+        int maxfreq  ;
+        char maxchar = ' '; //char that has the max frequency 
+
+        for(int i=0;i<s.size();i++){
+            maxfreq = 0 ;
+            maxchar = prevchar ;
+            for(int j=0;j<26;j++){
+                if(maxfreq<freq[j] && j != prevchar-'a'){
+                    maxfreq = freq[j] ;
+                    maxchar = j + 'a';
+                }
+            }
+            if(prevchar == maxchar) return "";
+            ans.push_back(maxchar) ;
+            freq[maxchar-'a']--;
+            prevchar = maxchar ;
+        }
+        return ans ;
+    }
+};
