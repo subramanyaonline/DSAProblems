cf9b85b33df7dd55137e85beff061a181612f672
subramanyaonline <subramanyanayak2003@gmail.com>
2026-05-12 16:08:33 +0530

    Time: 7 ms (87.67%) | Memory: 14.1 MB (33.29%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -4,7 +4,10 @@ public:
 
         sort(nums.begin(),nums.end()) ;
 
-        int closestSum = 1000000001 ; //int max might overflow when closestSum-target(if target is negative .)
+        //int closestSum = 1000000001 ; 
+        int closestSum = nums[0]+nums[1]+nums[2] ;
+        //int max might overflow when closestSum-target(if target is negative .)
+        //or better option is just initialize with first three elements sum given nums has minimum 3 elements . 
 
         for(int i=0;i<nums.size()-2;i++){
 
