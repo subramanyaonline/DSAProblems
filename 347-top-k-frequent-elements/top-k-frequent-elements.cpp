2265c3d6d02005720e262de5c03ca5e874fe2b14
subramanyaonline <subramanyanayak2003@gmail.com>
2026-05-29 11:04:20 +0530

    Time: 3 ms (50.52%) | Memory: 18.1 MB (32.07%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -3,6 +3,36 @@ public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
 
         unordered_map<int,int> um ;
+        for(int i=0;i<nums.size();i++){
+            if(um.count(nums[i])) um[nums[i]]++ ;
+            else um[nums[i]] = 1 ;
+        }
+
+        vector<int> frequencies; 
+        for(pair<int,int> p : um) frequencies.push_back(p.second) ;
+
+        priority_queue<int> pq(frequencies.begin(),frequencies.end()) ;
+
+        int cutoff ;
+        while(k>0){
+            cutoff = pq.top(); 
+            pq.pop() ;
+            k-- ;
+        }
+
+        vector<int> ans ; 
+        for(pair<int,int> p : um){
+            if(p.second >= cutoff) ans.push_back(p.first) ;
+        }
+
+        return ans ;
+    }
+};
+
+
+//takes n log n time with O(n) space. 
+/*
+unordered_map<int,int> um ;
         vector<int> frequencies ;
         vector<int> ans ;
 
@@ -24,5 +54,6 @@ public:
         }
 
         return ans ;
-    }
-};
+
+
+*/
