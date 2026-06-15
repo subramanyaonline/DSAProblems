da4055960f8425fae8a514438fabde65a7213137
subramanyaonline <subramanyanayak2003@gmail.com>
2026-06-15 23:23:58 +0530

    Time: 1 ms (33.25%) | Memory: 8.7 MB (83.35%) - LeetSync

--- 4303-count-k-th-roots-in-a-range/count-k-th-roots-in-a-range.cpp
+++ 4303-count-k-th-roots-in-a-range/count-k-th-roots-in-a-range.cpp
@@ -1,13 +1,26 @@
 class Solution {
 public:
     int countKthRoots(int l, int r, int k) {
+        if(k==1) return r-l+1 ;
 
-        if(l==30 && r==64 && k==3) return 1 ;
-        if(l==15 && r==64 && k==3) return 2 ;
-        int lower = ceil(pow(l,1.0/k)) ;
-        int upper = floor(pow(r,1.0/k)) ;
-        cout<<"lower"<<lower<<endl ;
-        cout<<"upper : "<<upper <<endl ;
-        return upper - lower + 1;
+        int count = 0 ;
+        for(int i=0;i<=10e5;i++){
+            int val = 1 ;
+            for(int j=1; j<=k;j++){
+                val = val*i ;
+            }
+            if(val>=l && val<=r) count++; 
+            else if(val>r) return count ;
+        }
+        return count ;
+
+
+        // if(l==30 && r==64 && k==3) return 1 ;
+        // if(l==15 && r==64 && k==3) return 2 ;
+        // int lower = ceil(pow(l,1.0/k)) ;
+        // int upper = floor(pow(r,1.0/k)) ;
+        // cout<<"lower"<<lower<<endl ;
+        // cout<<"upper : "<<upper <<endl ;
+        // return upper - lower + 1;
     }
 };
