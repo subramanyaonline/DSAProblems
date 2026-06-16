6d242e286f840ef14dabec1e770927bdc6731df5
subramanyaonline <subramanyanayak2003@gmail.com>
2026-06-16 16:42:18 +0530

    Time: 29 ms (41.68%) | Memory: 18.8 MB (36.72%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size())) ;
-        int noOfIslands = 0 ;
+        vector<vector<bool>> visited(grid.size(),vector<bool>(grid[0].size())) ;
+        int noOfIslands = 0 ; 
+        vector<pair<int,int>> directions = {{-1,0},{1,0},{0,-1},{0,1}} ;
+        queue<pair<int,int>> q ;
 
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
                 if(!visited[i][j] && grid[i][j]=='1'){
-                    dfs(grid,i,j,visited) ;
                     noOfIslands++ ;
+                    visited[i][j] = true ;
+                    q.push(make_pair(i,j)) ; 
+
+                    while(!q.empty()){
+
+                        int x = q.front().first ;
+                        int y = q.front().second ;
+                        q.pop() ;
+
+                        for(pair<int,int> p : directions){
+                            int dx = p.first + x ;
+                            int dy = p.second + y ;
+                            if(dx>=0&&dx<grid.size()&&dy>=0&&dy<grid[0].size()&&!visited[dx][dy]&&grid[dx][dy]=='1'){
+                                q.push(make_pair(dx,dy)) ;
+                                visited[dx][dy] = true ;
+                            }
+                           
+                        }
+
+                    }
+
                 }
             }
         }
         return noOfIslands ;
     }
-
-    void dfs(vector<vector<char>> &grid, int i, int j , vector<vector<bool>> &visited ){
-        if(i<0||i>=grid.size()||j<0||j>=grid[0].size()||visited[i][j]||grid[i][j]=='0') return ;
-        visited[i][j] = true ;
-        dfs(grid,i+1,j,visited) ;
-        dfs(grid,i-1,j,visited) ;
-        dfs(grid,i,j+1,visited) ;
-        dfs(grid,i,j-1,visited) ;
-    }
-};
+}; 
