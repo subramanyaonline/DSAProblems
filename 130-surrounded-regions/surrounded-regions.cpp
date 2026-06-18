a5f3b8451f6504cbd6ab4b4607cf757cf49fbe1b
subramanyaonline <subramanyanayak2003@gmail.com>
2026-06-18 14:26:24 +0530

    Time: 5 ms (8.66%) | Memory: 21.6 MB (5.12%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,21 +1,10 @@
 class Solution {
 public:
-    bool ifSurrounded(int i, int j, vector<vector<char>>& board,
-                      vector<vector<bool>>& visited) {
+    bool ifSurrounded(int i, int j , vector<vector<char>> &board, vector<vector<bool>> &visited){
+        if(i>=board.size()||i<0||j<0||j>=board[0].size()) return false ;
+        if(board[i][j]=='X' || visited[i][j]) return true ;
 
-        // Went outside ⇒ region touches boundary
-        if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size())
-            return false;
-
-        // X acts as a wall
-        if (board[i][j] == 'X')
-            return true;
-
-        // Already explored during this DFS
-        if (visited[i][j])
-            return true;
-
-        visited[i][j] = true;
+        visited[i][j] = true; 
 
         bool down = ifSurrounded(i + 1, j, board, visited);
         bool up = ifSurrounded(i - 1, j, board, visited);
@@ -25,36 +14,24 @@ public:
         return down && up && right && left;
     }
 
-    void change(int i, int j, vector<vector<char>>& board) {
-
-        if (i < 0 || i >= board.size() ||
-            j < 0 || j >= board[0].size() ||
-            board[i][j] == 'X')
-            return;
-
-        board[i][j] = 'X';
-
-        change(i + 1, j, board);
-        change(i - 1, j, board);
-        change(i, j + 1, board);
-        change(i, j - 1, board);
+    void change(int i, int j, vector<vector<char>> &board){
+        if(i>=board.size()||i<0||j<0||j>=board[0].size()||board[i][j]=='X')return ;
+        board[i][j] = 'X' ;
+        change(i+1,j,board) ;
+        change(i,j+1,board) ; 
+        change(i,j-1,board) ;
+        change(i-1,j,board) ; 
+        return ;
     }
 
+
     void solve(vector<vector<char>>& board) {
-
-        int m = board.size();
-        int n = board[0].size();
-
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-
-                // Process each component only once
-                if (board[i][j] == 'O' && !visited[i][j]) {
-
-                    if (ifSurrounded(i, j, board, visited)) {
-                        change(i, j, board);
+        vector<vector<bool>> visited(board.size(),vector<bool>(board[0].size())) ;
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                if(board[i][j]=='O' && !visited[i][j]){
+                    if(ifSurrounded(i,j,board,visited)){
+                        change(i,j,board) ; 
                     }
                 }
             }
