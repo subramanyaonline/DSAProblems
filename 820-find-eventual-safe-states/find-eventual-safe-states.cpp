9fef60a272b60cd7ccb817b2f27c14951aa0f56e
subramanyaonline <subramanyanayak2003@gmail.com>
2026-06-20 22:50:34 +0530

    Time: 10 ms (69.07%) | Memory: 51.3 MB (96.60%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
+    bool dfsIsCycle(int src, vector<vector<int>> &graph, vector<bool> &path, vector<bool> &safe){
+        if(path[src]) return true ;
+        if(safe[src]) return false  ; //already confirmed, memoized
+
+        path[src] = true ; 
+        for(int dest : graph[src]){
+            if(dfsIsCycle(dest,graph,path,safe)){
+                return true ;
+            }
+        }
+        safe[src] = true ; 
+        path[src] = false ; 
+        return false ;
+    }
+
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        vector<int> ans ; 
-        queue<int> q ; 
         int V = graph.size(); 
-        vector<int> indegree(V) ; 
-        vector<vector<int>> adjlist(V); 
-
-        for(int i=0;i<V;i++){       //here i itself is a node, which i took as destination . 
-            for(int src : graph[i]){
-                adjlist[src].push_back(i) ;
-                indegree[i]++; 
-            }
-        }
+        vector<bool> path(V) ; 
+        vector<bool> safe(V) ; 
+        vector<int> ans ; 
 
         for(int i=0;i<V;i++){
-            if(!indegree[i]){
-                q.push(i); 
-            }
-        }
-
-        while(!q.empty()){
-            int src = q.front(); 
-            q.pop(); 
-
-            for(auto dest : adjlist[src]){
-                if(!--indegree[dest])q.push(dest) ; 
-            }
-        }
+            dfsIsCycle(i,graph,path,safe) ; 
+        }       
 
         for(int i=0;i<V;i++){
-            if(!indegree[i]) ans.push_back(i) ; 
+            if(safe[i]) ans.push_back(i) ; 
         }
 
         return ans ;
