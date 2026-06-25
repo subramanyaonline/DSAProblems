6fcf3ac2ba0e995d8902e7d14d9e1e0dd1317d9c
subramanyaonline <subramanyanayak2003@gmail.com>
2026-06-25 10:12:44 +0530

    Time: 81 ms (65.53%) | Memory: 44.3 MB (81.75%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,46 +1,38 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        int INF = 6000*100 + 1; 
-        vector<int> time(n,INF) ; //visited not required as inf will be considered as not visited and any other value is considered as visited. 
-
+        const int INF = 6000 * 100 + 1 ; 
         vector<vector<pair<int,int>>> adjlist(n) ; 
-
-        for(int i=0;i<times.size();i++){
-            int from = times[i][0] ; 
-            int to = times[i][1] ; 
-            int t = times[i][2] ; 
-            adjlist[from-1].push_back({to-1,t});
+        for(int i=0;i<times.size();++i){
+            adjlist[times[i][0]-1].push_back({times[i][1]-1,times[i][2]}); 
         }
 
-        priority_queue<pair<int,int>> pq ; //pair stores node and weight . 
-        
-        pq.push({INF,k-1}) ; //first - how much it changes , second - node
-        time[k-1] = 0 ;  
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        vector<int> minTime(n,INF) ; 
+
+        pq.push({0,k-1}) ; // pathcost , node
+        minTime[k-1]=0 ; 
 
         while(!pq.empty()){
-            int src = pq.top().second; //to make the src 0 indexed. it is 1 indexed in the question . 
-            
+            int src = pq.top().second ; 
+            int time = pq.top().first ; 
             pq.pop(); 
-             
-
             for(auto p : adjlist[src]){
-                int dest = p.first; 
-                int wOrTime = p.second; 
-                if(time[src]+wOrTime  < time[dest]){
-                    pq.push({time[dest]-time[src]+wOrTime, dest}) ; 
-                    time[dest] = time[src]+wOrTime  ; 
+                int dest = p.first ; 
+                if(time+p.second < minTime[dest]){
+                    minTime[dest] = time+p.second ; 
+                    pq.push({minTime[dest],dest}); 
                 }
             }
-
         }
 
-        int maxtime = 0 ; 
+        int maxtime = 0 ;  
         for(int i=0;i<n;i++){
-            if(time[i]==INF) return -1 ; 
-            maxtime = max(maxtime,time[i]) ; 
+            if(minTime[i]==INF) return -1 ; 
+            maxtime = max(maxtime,minTime[i]); 
         }
-        
+
         return maxtime ; 
+
     }
 };
