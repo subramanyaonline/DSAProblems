61a188240fc389f88ae34a08b2e08026a9c7243e
subramanyaonline <subramanyanayak2003@gmail.com>
2026-06-26 23:37:25 +0530

    Time: 3 ms (85.93%) | Memory: 42.2 MB (74.69%) - LeetSync

---
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    struct DSU{
+        vector<int> parent ; 
+        vector<int> rank ; 
+        public: 
+        DSU(int n){
+            parent.resize(n,0); 
+            rank.resize(n,0) ; 
+            for(int i=0;i<n;i++) parent[i] = i ; 
+        }
+
+        int find(int node){
+            if(parent[node]==node) return node ; 
+            return parent[node] = find(parent[node]) ; 
+        }
+
+        int unionByRank(int a,int b){
+            int root1 = find(a) ; 
+            int root2 = find(b) ; 
+            if(root1==root2) return false ; 
+
+            if(rank[root1]==rank[root2]){
+                rank[root1]++; 
+                parent[root2] = root1 ; 
+            }else if(rank[root1]>rank[root2]){
+                parent[root2] = root1 ;
+            }else{
+                parent[root1] = root2 ; 
+            }
+            return true ; 
+        }
+    }; 
+
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        DSU dsu(n) ; 
+        int components = n ; 
+        int extraEdges = 0 ; 
+
+        for(int i=0;i<connections.size();i++){
+            int a = connections[i][0]; 
+            int b = connections[i][1] ; 
+            if(dsu.unionByRank(a,b)){
+                --components ; 
+            }else{
+                ++extraEdges ; 
+            }
+        }
+        cout<<components<<" "<<extraEdges<<endl ;
+        return components-1<=extraEdges ? components-1 : -1 ; 
+    }
+};
