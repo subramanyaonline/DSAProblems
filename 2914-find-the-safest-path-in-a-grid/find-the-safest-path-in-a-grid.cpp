d1c73c1393717033651a56ef1aa548944a44f29a
subramanyaonline <subramanyanayak2003@gmail.com>
2026-07-01 11:03:08 +0530

    Time: 282 ms (92.74%) | Memory: 119.2 MB (98.36%) - LeetSync

---
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int maximumSafenessFactor(vector<vector<int>>& grid) {
+        int n = grid.size(); 
+        vector<vector<int>> dp(n+2,vector<int>(n+2,800)) ; //stores minlen btw cells and theifcells
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(grid[i-1][j-1]) dp[i][j] = 0 ; 
+                else dp[i][j] = min(dp[i-1][j],dp[i][j-1])+1 ; 
+            }
+        }
+
+        for(int i=n;i>=1;i--){
+            for(int j=n;j>=1;j--){
+                if(grid[i-1][j-1]) dp[i][j] = 0 ; 
+                else dp[i][j] = min(dp[i][j],min(dp[i+1][j],dp[i][j+1])+1) ; 
+            }
+        }
+
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n;j++){
+        //         cout<<dp[i][j]<<" "; 
+        //     }
+        //     cout<<endl ;
+        // }
+
+        priority_queue<tuple<int,int,int>> pq ; //
+        vector<pair<int,int>> dir = {{1,0},{-1,0},{0,1},{0,-1}} ; 
+        vector<vector<int>> maxDist(n,vector<int>(n,-1)) ; 
+
+        pq.push({dp[1][1],0,0}) ; 
+        maxDist[0][0] = dp[1][1] ; 
+
+        while(!pq.empty()){
+            auto [safeness,x,y] = pq.top() ; 
+            pq.pop() ; 
+            //cout<<x<<" "<<y ; 
+            if(maxDist[x][y]>safeness) continue ; 
+            if(x==n-1 && y==n-1) return safeness ; 
+
+            for(int i=0;i<4;i++){
+                int dx = dir[i].first + x ; 
+                int dy = dir[i].second + y ; 
+
+                if(dx<n && dy<n && dx>=0 && dy>=0){
+                    int currSafeness = min(dp[dx+1][dy+1],safeness) ;
+                    if(currSafeness>maxDist[dx][dy]){
+                        //cout<<"dx: "<<dx<<"   "<<"dy:"<<dy<<"   //cursafeness"<<currSafeness<< endl ; 
+                        pq.push({currSafeness,dx,dy}) ; 
+                        maxDist[dx][dy] = currSafeness ;
+                    }                   
+                }
+
+            }
+        }
+
+        return maxDist[n-1][n-1] ; 
+    }
+};
