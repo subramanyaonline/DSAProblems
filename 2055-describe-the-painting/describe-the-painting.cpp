9f5e21041bcbf274608f9bac85b9d213b0898864
subramanyaonline <subramanyanayak2003@gmail.com>
2026-07-05 22:14:55 +0530

    Time: 126 ms (23.60%) | Memory: 108.5 MB (54.61%) - LeetSync

--- 2055-describe-the-painting/describe-the-painting.cpp
+++ 2055-describe-the-painting/describe-the-painting.cpp
@@ -2,43 +2,28 @@ class Solution {
 public:
 
     vector<vector<long long>> splitPainting(vector<vector<int>>& segments) {
-        int n = 0; 
+        map<int,long long> map ; 
+
         for(int i=0;i<segments.size();i++){
-            n = max(n,segments[i][1]); 
+            int from = segments[i][0], to = segments[i][1] , color = segments[i][2] ; 
+            map[from] = map[from] + color ; 
+            map[to] = map[to] - color ; 
         }
 
-        vector<long long > diffarr(n+1) ; 
-        vector<bool> ends(n+1) ; 
-        
-        for(int i=0 ; i<segments.size();i++){
-            int from = segments[i][0] ; 
-            int to = segments[i][1] ; 
-            int color = segments[i][2] ; 
-            diffarr[from] = diffarr[from] + color ;
-            ends[from] = ends[to] = true ; 
-            diffarr[to] = diffarr[to] - color ;  
-        }
-
-        long long prevcolor = diffarr[0] ; 
-        int segment1 = 0 ; //start segment 
+        long long sum = 0 ; 
+        int prevpoint = -1 ; 
         vector<vector<long long>> ans ; 
-        for(int i=1;i<=n;i++){
-            diffarr[i] = diffarr[i-1] + diffarr[i] ; 
-            long long curcolor = diffarr[i] ; 
-                 
-            if(ends[i] && prevcolor!=0){
-                ans.push_back({segment1,i,prevcolor}); 
-                segment1 = i ; 
-                prevcolor = diffarr[i] ; 
-            }else if(ends[i] && prevcolor==0){
-                segment1 = i ; 
-                prevcolor = diffarr[i] ; 
+        for(auto interval : map){
+            long long  color = interval.second ; 
+            int endpoint = interval.first ;  
+            if(sum>0){               
+                ans.push_back({prevpoint,endpoint,sum}) ;   
             }
-
+            prevpoint = endpoint ;
+            sum += color ; 
         }
 
-        //if(prevcolor!=0) ans.push_back({segment1,n,prevcolor}); 
+        return ans ; 
 
-        return ans ;
     }
 };
