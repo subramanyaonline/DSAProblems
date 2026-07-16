7767fb4561632dda1f99a5e7d2ea8c9998570780
subramanyaonline <subramanyanayak2003@gmail.com>
2026-07-16 21:17:27 +0530

    Time: 64 ms (90.78%) | Memory: 155.6 MB (87.10%) - LeetSync

--- 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.cpp
+++ 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
-    int gcd(int a, int b) {
-        if(a==0) return b ; 
-        if(b==0) return a ; 
-        return gcd(b,a%b) ; 
-    }
+    
     long long gcdSum(vector<int>& nums) {
         int n = nums.size() ; 
         vector<int> prefixgcd(n) ; 
@@ -12,14 +8,35 @@ public:
 
         for(int i=0;i<n;i++){
             maxele = max(maxele,nums[i]) ; 
-            prefixgcd[i] = gcd(nums[i],maxele) ; 
+            //prefixgcd[i] = gcd(nums[i],maxele) ; 
+            int a = maxele ;
+            int b = nums[i] ; 
+            while(true){
+                if(a==0){prefixgcd[i] = b ; break ; }
+                if(b==0){prefixgcd[i] = a ; break ; }
+                int temp = a ; 
+                a = b ; 
+                b = temp%b ; 
+            }
         }
+
 
         sort(prefixgcd.begin(),prefixgcd.end()) ; 
 
         long long totgcd = 0 ; 
         for(int i=0;i<n/2;i++){
-            totgcd += gcd(prefixgcd[i],prefixgcd[n-1-i]) ; 
+            int a = prefixgcd[n-1-i] ;
+            int b = prefixgcd[i] ;
+            int gcd ;  
+            while(true){
+                if(a==0){ gcd = b ; break ; }
+                if(b==0){ gcd = a ; break ; }
+                int temp = a; 
+                a = b ; 
+                b = temp%b ; 
+            }
+
+            totgcd += gcd ; 
         }
 
         return totgcd ; 
