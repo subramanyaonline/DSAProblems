3dbbf85b093ced1c724f15818c2320b1aca6387a
subramanyaonline <subramanyanayak2003@gmail.com>
2026-07-24 02:52:26 +0530

    Time: 2183 ms (5.00%) | Memory: 69 MB (74.13%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,15 +1,30 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int> pq ; 
-        for(int i=0;i<nums.size();i++){
-            pq.push(nums[i]) ;
+        int left = 0 , right = nums.size()-1 ; 
+        int targetpos = k-1 ; 
+        
+        while(left<=right){ //because we are returning inside while, <=, only breaks once found the pos which lies in the range left-right for sure . 
+            int pivot = nums[right] ; 
+            int p = left ; 
+
+            for(int i=left;i<right;i++){ //we are finding the correct postion for pivot. 
+                if(nums[i]>pivot){  //if we found any smaller ele than pivot .
+                    swap(nums[i],nums[p]); 
+                    p++ ; 
+                }
+            }
+
+            swap(nums[p],nums[right]) ; //giving pivot the correct postion which is p 
+
+            if(p==targetpos){ //target = k-1 ; 0 indexed right . so 
+                return nums[p] ; 
+            }else if(p<targetpos){  //target pos is in the right part 
+                left = p+1 ; 
+            }else{  //target pos is in the left part .
+                right = p-1 ; 
+            }
         }
-        int val ; 
-        for(int i=0;i<k;i++){
-            val = pq.top(); 
-            pq.pop(); 
-        }
-        return val ; 
+        return -1 ; 
     }
 };
