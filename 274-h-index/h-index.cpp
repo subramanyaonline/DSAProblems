8c32a32f269e2960c12063ce6063acded755fe11
subramanyaonline <subramanyanayak2003@gmail.com>
2026-08-15 20:12:47 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (15.38%) - LeetSync

--- 274-h-index/h-index.cpp
+++ 274-h-index/h-index.cpp
@@ -2,12 +2,24 @@ class Solution {
 public:
     int hIndex(vector<int>& citations) {
         int n = citations.size() ; 
-        sort(citations.begin(),citations.end()) ; 
-        
-        for(int i=0; i<n; ++i){
-            int hindex = n-i ; //count
-            if(hindex<=citations[i]) return hindex ; 
+        vector<int> prearr(n+1) ; 
+
+        for(int i=0;i<n;i++){
+            int ele = citations[i] ; 
+            if(ele>n) prearr[n]++ ; 
+            else prearr[ele]++ ; 
         }
+        //calculates how many elements in the array are greater than i index 
+        for(int i=n-1;i>=0;--i){
+            prearr[i] = prearr[i+1] + prearr[i] ;  
+        }
+
+        //to find the hindex (max) 
+        for(int i=n;i>=0;--i){
+            if(i<=prearr[i]) return i ; 
+        }
+
+        //doesnot run at all , 
         return 0 ; 
     }
 };
