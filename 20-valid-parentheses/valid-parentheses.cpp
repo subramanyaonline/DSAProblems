cf8d8618fbd7007e806f8526f8b4ea76373397b7
subramanyaonline <subramanyanayak2003@gmail.com>
2026-08-23 08:05:40 +0530

    Time: 2 ms (15.01%) | Memory: 9.9 MB (8.67%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     bool isValid(string s) {
-        unordered_map<char,char> map = {
-            { '{' , '}' }, 
-            { '(' , ')' },
-            { '[' , ']' }
-        }; 
-
         stack<char> st ; 
 
         for(char c : s){
-            if(map.contains(c)) st.push(c) ; 
-            else {
-                if(st.empty() || map[st.top()] != c) return false ; 
+            if(c=='{'){
+                st.push('}'); 
+            }else if(c=='('){
+                st.push(')'); 
+            }else if(c=='['){
+                st.push(']'); 
+            }else{
+                if(st.empty() || st.top()!= c) return false ; 
                 st.pop() ; 
             }
         }
