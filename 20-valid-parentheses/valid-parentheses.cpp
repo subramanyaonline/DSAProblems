247437c419899f217918193a5977b5a6501770c5
subramanyaonline <subramanyanayak2003@gmail.com>
2026-08-23 08:01:55 +0530

    Time: 3 ms (9.19%) | Memory: 9.1 MB (13.70%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
     bool isValid(string s) {
-        unordered_map<char,int> open = {{'(',1},{'[',2},{'{',3}} ; 
-        unordered_map<char,int> close = {{')',1},{']',2},{'}',3}} ; 
+        unordered_map<char,char> map = {
+            { '{' , '}' }, 
+            { '(' , ')' },
+            { '[' , ']' }
+        }; 
+
         stack<char> st ; 
 
-        int size = s.size() ; 
-        
-        for(int i=0;i<size;++i){
-
-            char curchar = s[i] ; 
-
-            if(open.contains(curchar)) st.push(curchar) ; 
-            
-            else{
-                //curchar is closing 
-               if(st.empty()) return false ; // for testcases like ']'
-
-               int curBracketNo = close[curchar] ; //barcket number 
-               int topBracketNo = open[st.top()] ; 
-
-               if( curBracketNo != topBracketNo ) return false ;
-               else st.pop() ; 
+        for(char c : s){
+            if(map.contains(c)) st.push(c) ; 
+            else {
+                if(st.empty() || map[st.top()] != c) return false ; 
+                st.pop() ; 
             }
         }
-        if(st.empty()) return true ; //actually for test cases like '(['
-        return false ; 
+
+        return st.empty() ; 
     }
 };
