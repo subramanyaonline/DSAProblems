5bca1151020ec94084c6744412ddffa199bd066f
subramanyaonline <subramanyanayak2003@gmail.com>
2026-09-04 14:43:43 +0530

    Time: 7 ms (63.00%) | Memory: 9.3 MB (79.85%) - LeetSync

--- 688-knight-probability-in-chessboard/knight-probability-in-chessboard.cpp
+++ 688-knight-probability-in-chessboard/knight-probability-in-chessboard.cpp
@@ -4,6 +4,7 @@ public:
 
         vector<vector<double>> prevdp(n+4,vector<double>(n+4)) ; 
         vector<vector<double>> currdp(n+4,vector<double>(n+4)) ; 
+        vector<pair<int,int>> dir = {{-2,-1},{-2,1},{-1,-2},{-1,2},{1,-2},{1,2},{2,-1},{2,1}}; 
 
 //base case , which is actually for 0 steps , what is the probability that the knight stays inside the board. which is 100% , ie , 1 probability . 
         for(int i=2;i<=n+1;++i){
@@ -16,11 +17,10 @@ public:
             for(int i=2;i<=n+1 ; ++i){
                 for(int j=2;j<=n+1;++j){
                     double prob = 0.0 ; 
-                    vector<pair<int,int>> dir = {{i-2,j-1},{i-2,j+1},{i-1,j-2},{i-1,j+2},{i+1,j-2},{i+1,j+2},{i+2,j-1},{i+2,j+1}}; 
 
                     for(auto p : dir){
-                        int dx = p.first ; 
-                        int dy = p.second ; 
+                        int dx = p.first + i; 
+                        int dy = p.second + j; 
                         prob += prevdp[dx][dy] * (double)1/8 ; 
                     }
 
