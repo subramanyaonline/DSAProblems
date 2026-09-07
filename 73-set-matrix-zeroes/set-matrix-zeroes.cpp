0125ef1dfd6eed567edfefda7e8e2f5ff40ec031
subramanyaonline <subramanyanayak2003@gmail.com>
2026-09-07 23:10:38 +0530

    Time: 0 ms (100.00%) | Memory: 20.9 MB (73.88%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -4,32 +4,38 @@ public:
         int m = matrix.size() ; 
         int n = matrix[0].size() ; 
 
-        vector<bool> rows(m) ; 
-        vector<bool> cols(n) ; 
+        bool firstrow = false ; 
+        bool firstcol = false ; 
 
         for(int i=0;i<m;++i){
             for(int j=0;j<n;++j){
                 if(!matrix[i][j]){
-                    rows[i] = true ; 
-                    cols[j] = true ;
+                    if(i==0) firstrow = true ; 
+                    if(j==0) firstcol = true ;
+                    matrix[i][0] = 0 ; 
+                    matrix[0][j] = 0 ;
                 }
             }
         }
 
-        for(int i=0;i<m;++i){
-            if(rows[i]){
-                for(int j=0;j<n;++j){
+        for(int i=1;i<m;++i){
+            if(!matrix[i][0]){
+                for(int j=0;j<n;++j){ //can also start from 1, doesnot matter
                     matrix[i][j] = 0 ; 
                 }
             }
         }
 
-        for(int j=0;j<n;++j){
-            if(cols[j]){
-                for(int i=0;i<m;++i){
+        for(int j=1;j<n;++j){
+            if(!matrix[0][j]){
+                for(int i=0;i<m;++i){ //can also start from 1, doesnot matter
                     matrix[i][j] = 0 ; 
                 }
             }
         }
+
+        if(firstrow) for(int j=0;j<n;++j) matrix[0][j] = 0 ;
+        if(firstcol) for(int i=0;i<m;++i) matrix[i][0] = 0 ; 
+
     }
 };
