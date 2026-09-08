9cb163ae2b449ba711161a62d0e0c3644b0454d8
subramanyaonline <subramanyanayak2003@gmail.com>
2026-09-08 18:23:56 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (85.19%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -5,26 +5,32 @@ public:
         int m = matrix.size() ; 
         int n = matrix[0].size() ; 
 
-        vector<vector<int>> dp(m,vector<int>(n)) ; 
+        vector<int> prevdp(n) ; 
+        vector<int> currdp(n) ; 
+
         int mincost = INT_MAX ; 
 
         //base case 
         for(int j=0;j<n;j++){
-            dp[0][j] = matrix[0][j] ; 
+            prevdp[j] = matrix[0][j] ; 
         }
 
         for(int i=1;i<m;i++){
-            dp[i][0] = min(dp[i-1][0] , dp[i-1][1]) + matrix[i][0] ;     
+            currdp[0] = min(prevdp[0] , prevdp[1]) + matrix[i][0] ;     
+
             for(int j=1;j<n-1;j++){
-                dp[i][j] = matrix[i][j] + min({dp[i-1][j-1],dp[i-1][j],dp[i-1][j+1]}); 
+                currdp[j] = matrix[i][j] + min({prevdp[j-1],prevdp[j],prevdp[j+1]}); 
             }
-            dp[i][n-1] = min(dp[i-1][n-1],dp[i-1][n-2]) + matrix[i][n-1] ; 
+            
+            currdp[n-1] = min(prevdp[n-1],prevdp[n-2]) + matrix[i][n-1] ; 
+
+            prevdp = currdp ; 
         }
 
         for(int j=0;j<n;++j){
-            mincost = min(mincost,dp[m-1][j]) ; 
+            mincost = min(mincost,prevdp[j]) ; 
         }
-        
+
         return mincost ; 
     }
 };
